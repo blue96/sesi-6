1f8e0a8ac2c585ce56e84b2c67d1606665072bb4
blue96 <pascalfelim@yahoo.com>
2020-05-09 13:57:32 +0700

    upload

---
+++ sesi 6 5/index.js
@@ -0,0 +1,6 @@
+let a = '';
+while(a.toUpperCase() !== 'INDONESIA ASAHAN ALUMINIUM'){
+    a = prompt('Sebutkan kepanjangan dari INALUM');
+}
+console.log('Selamat jawaban kamu benar');
+document.write('Selamat jawaban kamu benar');
